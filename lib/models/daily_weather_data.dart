db774a792d95b43dde3dd38f881831a40e7ee826
Nate Mallison <healthynate@gmail.com>
2022-01-04 15:19:58 -0500

    Created flutter test.
    Fixed extra folder.

---
+++ lib/models/daily_weather_data.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+class DailyWeatherData {
+  final DateTime timestamp;
+  final int temperatureLow;
+  final int tempuratureHigh;
+  final int windHigh;
+  final int windLow;
+  final double preciptiation;
+  final int humidityHigh;
+  final int humidityLow;
+
+  DailyWeatherData({
+    required this.timestamp,
+    required this.tempuratureHigh,
+    required this.temperatureLow,
+    required this.windHigh,
+    required this.windLow,
+    required this.preciptiation,
+    required this.humidityHigh,
+    required this.humidityLow,
+  });
+
+  static DailyWeatherData genDailyWeather(DateTime timestamp) {
+    return DailyWeatherData(
+      timestamp: timestamp,
+      tempuratureHigh: Random().nextInt(25),
+      temperatureLow: Random().nextInt(12),
+      windHigh: Random().nextInt(18),
+      windLow: Random().nextInt(4),
+      preciptiation: (Random().nextDouble() * 4),
+      humidityHigh: Random().nextInt(22),
+      humidityLow: Random().nextInt(8),
+    );
+  }
+
+  static Future<List<DailyWeatherData>> getDailyWeatherData() {
+    final today = DateTime.now();
+    final tomorrow = DateTime.now().add(const Duration(days: 1));
+    final yesterday = DateTime.now().subtract(const Duration(days: 1));
+    final twoDaysAgo = DateTime.now().subtract(const Duration(days: 2));
+
+    final dailyWeather = [
+      genDailyWeather(tomorrow),
+      genDailyWeather(today),
+      genDailyWeather(yesterday),
+      genDailyWeather(twoDaysAgo),
+    ];
+
+    return Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        return dailyWeather;
+      },
+    );
+  }
+}
